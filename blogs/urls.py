4acdacc3fdce1bbf309a2b4d07fc5ff37beb4128
Bahar <ibahare05@gmail.com>
2025-10-12 14:04:02 +0330

    filtering base on writer of the posts

--- blogs/urls.py
+++ blogs/urls.py
@@ -7,6 +7,7 @@ app_name = "blogs"
 urlpatterns = [
     path("blog/",views.blog_home, name="blog_home"),
     path("blog/category/<slug:slug>/",views.blog_home, name= "category"),
+    path("blog/author/<str:username>/", views.blog_home, name="author"),
     path("blog/<slug:slug>/",views.blog_single, name="blog_single"),
     
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import get_object_or_404, render
 from .models import *
 
-def blog_home(request, slug=None):
+def blog_home(request, slug=None,username=None):
     posts=Post.objects.filter(status="published")
 
     category = None
+    author = None
 
     if slug:  # اگر slug دسته داده شده
         category = get_object_or_404(Category, slug=slug)
         posts = posts.filter(category=category)
+
+    if username:
+        author = get_object_or_404(User, username=username)
+        posts = posts.filter(author=author)
+
+    
     context={
         "posts":posts,
         "category":category,
+        "author": author,
     }
     return render(request,"blogs/blog_home.html", context)
 
