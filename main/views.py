8761369640c6b02777f49fa99cdcc4691b0b4211
Bahar <ibahare05@gmail.com>
2025-10-16 12:29:08 +0330

    pop up notifications

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import redirect, render
 from .forms import *
+from django.contrib import messages
 
 def index_view(request):
     return render(request, "main/index.html")
@@ -13,6 +14,24 @@ def contact_view(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
+            if request.headers.get("x-requested-with") == "XMLHttpRequest":
+                return JsonResponse({
+                    "success": True,
+                    "message": "Your message has been sent successfully✅"
+                })
+
+            # ✅ اگر معمولی بود (رفرش صفحه)
+            messages.success(request, "Your message has been sent successfully✅")
+            return redirect("main:contact")
+        else:
+            if request.headers.get("x-requested-with") == "XMLHttpRequest":
+                return JsonResponse({
+                    "success": False,
+                    "message": "Please fill in all fields correctly "
+                })
+
+            messages.error(request, "Please fill in all fields correctly ❌")
+            return redirect("main:contact")
 
     form = ContactForm()
     return render(request, 'main/contact.html', {'form': form})
@@ -23,7 +42,13 @@ def newsletter_view(request):
         form = NewsLetterForm(request.POST)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect("/")
+            messages.success(request, "✅ You have successfully subscribed to the newsletter!")
+            # پاسخ برای AJAX
+            if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+                return JsonResponse({'success': True, 'message': "You have successfully subscribed!"})
+            return redirect('/')
         else:
-            return HttpResponseRedirect("/")
-    
+            messages.error(request, "❌ Invalid email address.")
+            if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+                return JsonResponse({'success': False, 'message': "Invalid email address."})
+            return HttpResponseRedirect('/')
